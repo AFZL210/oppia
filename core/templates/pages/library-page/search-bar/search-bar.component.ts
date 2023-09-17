3da24eeb38284b774d5563bcdd66f53954ffbd6d
AFZL210 <tricktomade@gmail.com>
2023-09-17 20:14:27 +0530

    fix lint

--- core/templates/pages/library-page/search-bar/search-bar.component.ts
+++ core/templates/pages/library-page/search-bar/search-bar.component.ts
@@ -198,9 +198,9 @@ export class SearchBarComponent implements OnInit, OnDestroy {
           this.searchQuery, this.selectionDetails.categories.selections,
           this.selectionDetails.languageCodes.selections
         );
-        if (!searchUrlQueryString){ 
+        if (!searchUrlQueryString) {
           return;
-        }
+        }        
         let url = new URL(this.windowRef.nativeWindow.location.toString());
         let siteLangCode: string | null = url.searchParams.get('lang');
         url.search = '?q=' + searchUrlQueryString;
